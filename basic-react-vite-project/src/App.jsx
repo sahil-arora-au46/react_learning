fa39a31b7d61523c9cc416584214c4c8952ea5a9
sahil <sahil@vidyamantra.com>
2023-09-10 18:03:40 +0530

    initial commit

---
+++ basic-react-vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <h1>hi from vite- react</h1>
+  )
+}
+
+export default App
---
+++ basicreactapp/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+   <h1>first react app</h1>
+  );
+}
+
+export default App;
